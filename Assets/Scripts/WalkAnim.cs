82a999d1547ee1a73d710d347ae73712300da794
Raimundo Cerda <rai.cerda@gmail.com>
2013-10-13 02:45:12 -0300

    First F*cking commit

---
+++ Assets/Scripts/WalkAnim.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WalkAnim : MonoBehaviour {
+	
+	public static bool facingRight;
+	public static bool isMovingRight;
+	public static bool facingLeft;
+	public static bool isMovingLeft;
+	public static bool jumping;
+	public static bool grappling;
+	public static bool stand;
+	public static int lastDir;
+	public static int dir;
+	public tk2dSpriteAnimator anim;
+	public tk2dSprite sprite;
+	
+	
+	// Use this for initialization
+	void Start () {
+		lastDir = 0;
+		dir = 0;
+		facingLeft=false;
+		isMovingRight = false;
+		facingRight=false;
+		isMovingLeft = false;
+		jumping = false;
+		grappling = false;
+		stand = true;
+		positioning(dir);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		positioning(dir);	
+	}
+	
+	void Anim () {
+			
+		if(facingRight && !isMovingRight){
+			isMovingRight = true;
+			isMovingLeft = false;
+			//TODO anim to right;
+		}
+		if(facingLeft && !isMovingLeft){
+			isMovingLeft = false;
+			isMovingLeft = true;
+		}
+		
+	}
+	
+	void positioning(int dirParam){
+		if(lastDir == dirParam){
+			
+		}else{
+			switch (dirParam){
+				case -1:
+				anim.Play("Walk");
+				sprite.scale = new Vector3(-1,1,1);
+				lastDir = dirParam;
+				break;
+				
+				case 1:
+				anim.Play("Walk");
+				sprite.scale = new Vector3(1,1,1);
+				lastDir = dirParam;
+				break;
+				
+				case 0:
+				anim.Stop();
+				lastDir = dirParam;
+				break;
+			}
+		}
+	}
+	
+	public static void setDir(int dirPara){
+		dir = dirPara;
+	}
+}
---
+++ Assets/Scripts/WalkController.cs
@@ -0,0 +1,110 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WalkController : MonoBehaviour {
+	public float speed = 6.0F;
+    public float jumpSpeed = 8.0F;
+    public float gravity = 20.0F;
+	public bool isAlive = true;
+	public bool onAir;
+	private float direction;
+	public bool isGrappable = false;
+    private Vector3 moveDirection = Vector3.zero;
+	public CharacterController controller;
+	public int distance;
+	
+	void Start(){
+		controller = GetComponent<CharacterController>();
+		direction = 1;
+		distance = 0;
+	}
+	
+    void Update() {
+		
+		float dirAux = Input.GetAxis("Horizontal");
+		whereIsFacing(dirAux);
+		
+		if (onAir){ distance++; Debug.Log(distance);}
+        if (controller.isGrounded) {
+			if (distance > 60) Debug.Log("DEAD BY FALL");
+			onAir = false;
+			distance = 0;
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+            if (Input.GetButton("Jump")){
+				onAir = true;
+                moveDirection.y = jumpSpeed;
+			}
+        }else{
+			if(Input.GetAxis("Horizontal")==0){
+				
+			}else{
+				moveDirection = new Vector3(Input.GetAxis("Horizontal"),moveDirection.y,0);
+				moveDirection = transform.TransformDirection(moveDirection);
+            	moveDirection.x *= speed;
+			}
+		}
+		if(Input.GetKey(KeyCode.Z)){
+			if(isGrappable){
+				moveDirection.Set(moveDirection.x,0,0);
+				onAir = false;
+				distance = 0;
+				if(Input.GetButton ("Jump")){
+					onAir = true;
+					moveDirection.y = jumpSpeed;
+					moveDirection.x = jumpSpeed*direction;
+				}
+			}
+		}
+		
+		moveDirection.Set(moveDirection.x,moveDirection.y,0);
+        if(isGrappable && Input.GetKey(KeyCode.Z)){}else{ moveDirection.y -= gravity * Time.deltaTime;}
+        controller.Move(moveDirection * Time.deltaTime);
+//		float dirAux = Input.GetAxis("Horizontal");
+//		whereIsFacing(dirAux);
+    }
+	
+	
+	
+	
+	// Check if the character can grapple onto a dirtWall
+	
+	void OnTriggerEnter(Collider target){
+		if (target.transform.tag.Equals("dirtWall")) {
+			isGrappable = true;
+			direction = Mathf.Sign (moveDirection.x)*-1;
+		}
+		if (target.transform.tag.Equals("spikes")){
+			Debug.Log("DEAD");
+			isAlive = false;
+		}
+		if (target.transform.tag.Equals("killerObject")){
+			target.transform.parent.rigidbody.velocity = new Vector3(0,-10,0);
+			Debug.Log("Caigo");
+		}
+		
+	}
+	
+	void OnTriggerExit(Collider target){
+		if (target.transform.tag.Equals("dirtWall")) {
+			isGrappable = false;
+		}
+	}
+	
+	void whereIsFacing(float dir){
+			
+		if(dir<0){
+			WalkAnim.setDir(-1);
+		}else{
+			if( dir>0){
+				WalkAnim.setDir(1);
+			}else{
+				WalkAnim.setDir(0);
+			}
+		}
+	}
+	
+	
+}
+
